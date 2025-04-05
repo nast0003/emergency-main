122bdbe5bfc78706d150a32fe520f39f9ff939f5
Nanna <nast0003@stud.kea.dk>
2025-04-05 22:52:06 +0200

    js

--- js/form.js
+++ js/form.js
@@ -1,18 +1,29 @@
 // Summary kode
 
 // console.log(document.querySelector("#name").value);
+//document.querySelector("#sum").style.display = "hidden";
 
 const submit = document.querySelector("#btn-indsend");
 const name = document.querySelector("#sumName");
 const email = document.querySelector("#sumEmail");
-const Lokation = document.querySelector("#sumLocation");
+const lokation = document.querySelector("#sumLocation");
 const beskrivelse = document.querySelector("#sumDescription");
 
-submit.addEventListener("click", submitForm);
+submit.onclick = (e) => {
+  e.preventDefault();
+  submitForm();
+};
 
 function submitForm() {
-  document.querySelector("sumName").textContent = document.querySelector("#name").value;
-  document.querySelector("sumEmail").textContent = document.querySelector("#email").value;
-  document.querySelector("sumLocation").textContent = document.querySelector("#Lokation").value;
-  document.querySelector("sumDescription").textContent = document.querySelector("beskrivelse").value;
+  document.querySelector("#sumName").innerHTML = document.querySelector("#name").value;
+  document.querySelector("#sumEmail").textContent = document.querySelector("#email").value;
+  document.querySelector("#sumLocation").textContent = document.querySelector("#lokation").value;
+  document.querySelector("#sumDescription").textContent = document.querySelector("beskrivelse").value;
+}
+
+document.querySelector("#reset-btn").addEventListener("click", reset);
+
+function reset() {
+  document.querySelector("#webform").reset();
+  document.querySelector("#sum").style.visibility = "hidden";
 }
