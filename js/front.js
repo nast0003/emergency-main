4bba546d66a56a0de51df11c70e09b8b372f0a70
Nanna <nast0003@stud.kea.dk>
2025-04-12 13:38:20 +0200

    opdaterer infografik

--- js/front.js
+++ js/front.js
@@ -1,59 +1,59 @@
 // Version 1: One breaking only
 // **********************************************
-const openBtn = document.querySelector(".news_card .cta_btn");
-const closeBtn = document.querySelector(".news_card dialog button");
-const dialog = document.querySelector(".news_card dialog");
-openBtn.addEventListener("click", openDialog);
-closeBtn.addEventListener("click", closeDialog);
+// const openBtn = document.querySelector(".news_card .cta_btn");
+// const closeBtn = document.querySelector(".news_card dialog button");
+// const dialog = document.querySelector(".news_card dialog");
+// openBtn.addEventListener("click", openDialog);
+// closeBtn.addEventListener("click", closeDialog);
 
-function openDialog() {
-  dialog.showModal();
-}
-function closeDialog() {
-  dialog.close();
-}
+// function openDialog() {
+//   dialog.showModal();
+// }
+// function closeDialog() {
+//   dialog.close();
+// }
 
 // **********************************************
 
 // Version 2: with 3 sets of functions to make 3 breaking news work
 // **********************************************
-// const openBtn1 = document.querySelector(".news_card:first-of-type .read-more");
-// const closeBtn1 = document.querySelector(".news_card:first-of-type dialog button");
-// const dialog1 = document.querySelector(".news_card:first-of-type dialog");
-// openBtn1.addEventListener("click", openDialog1);
-// closeBtn1.addEventListener("click", closeDialog1);
+const openBtn1 = document.querySelector(".news_card:first-of-type .cta_btn");
+const closeBtn1 = document.querySelector(".news_card:first-of-type dialog button");
+const dialog1 = document.querySelector(".news_card:first-of-type dialog");
+openBtn1.addEventListener("click", openDialog1);
+closeBtn1.addEventListener("click", closeDialog1);
 
-// function openDialog1() {
-//   dialog1.showModal();
-// }
-// function closeDialog1() {
-//   dialog1.close();
-// }
+function openDialog1() {
+  dialog1.showModal();
+}
+function closeDialog1() {
+  dialog1.close();
+}
 
-// const openBtn2 = document.querySelector(".news_card:nth-child(2) .read-more");
-// const closeBtn2 = document.querySelector(".news_card:nth-child(2) dialog button");
-// const dialog2 = document.querySelector(".news_card:nth-child(2) dialog");
-// openBtn2.addEventListener("click", openDialog2);
-// closeBtn2.addEventListener("click", closeDialog2);
+const openBtn2 = document.querySelector(".news_card:nth-child(2) .cta_btn");
+const closeBtn2 = document.querySelector(".news_card:nth-child(2) dialog button");
+const dialog2 = document.querySelector(".news_card:nth-child(2) dialog");
+openBtn2.addEventListener("click", openDialog2);
+closeBtn2.addEventListener("click", closeDialog2);
 
-// function openDialog2() {
-//   dialog2.showModal();
-// }
-// function closeDialog2() {
-//   dialog2.close();
-// }
-// const openBtn3 = document.querySelector(".news_card:nth-child(3) .read-more");
-// const closeBtn3 = document.querySelector(".news_card:nth-child(3) dialog button");
-// const dialog3 = document.querySelector(".news_card:nth-child(3) dialog");
-// openBtn3.addEventListener("click", openDialog3);
-// closeBtn3.addEventListener("click", closeDialog3);
+function openDialog2() {
+  dialog2.showModal();
+}
+function closeDialog2() {
+  dialog2.close();
+}
+const openBtn3 = document.querySelector(".news_card:nth-child(3) .cta_btn");
+const closeBtn3 = document.querySelector(".news_card:nth-child(3) dialog button");
+const dialog3 = document.querySelector(".news_card:nth-child(3) dialog");
+openBtn3.addEventListener("click", openDialog3);
+closeBtn3.addEventListener("click", closeDialog3);
 
-// function openDialog3() {
-//   dialog3.showModal();
-// }
-// function closeDialog3() {
-//   dialog3.close();
-// }
+function openDialog3() {
+  dialog3.showModal();
+}
+function closeDialog3() {
+  dialog3.close();
+}
 // **********************************************
 
 // Version 3: Using querySelectorAll and forEach to make it work for several breaking news
--- js/main.js
+++ js/main.js
@@ -1,19 +1,60 @@
-document.querySelector("#head_circle").addEventListener("mouseover", headHighlight);
-document.querySelector("#head_circle").addEventListener("mouseout", headunHighlight);
-document.querySelector("#head_circle").addEventListener("click", headInfo);
+// document.querySelector("#circle1").addEventListener("mouseover", headHighlight);
+// document.querySelector("#circle1").addEventListener("mouseout", headunHighlight);
+// document.querySelector("#circle1").addEventListener("click", headInfo);
 
-function headHighlight() {
-  console.log("headHighlight");
-  document.querySelector("#head_circle").style.fill = "white";
-}
+// function headHighlight() {
+//   console.log("headHighlight");
+//   document.querySelector("#circle1").style.fill = "white";
+// }
 
-function headunHighlight() {
-  console.log("headunHighlight");
-  document.querySelector("#head_circle").style.fill = "red";
-}
+// function headunHighlight() {
+//   console.log("headunHighlight");
+//   document.querySelector("#circle1").style.fill = "red";
+// }
 
-function headInfo() {
-  console.log("headInfo");
-  document.querySelector(".info-text > h2").textContent = "Hovedet!";
-  document.querySelector(".info-text > article > p").textContent = "lorem ipsum";
-}
+// function headInfo() {
+//   console.log("headInfo");
+//   document.querySelector(".info-text > h2").textContent = "Hovedet!";
+//   document.querySelector(".info-text > article > p").textContent = "lorem ipsum";
+// }
+
+const circles = document.querySelectorAll("circle");
+
+circles.forEach((circle) => {
+  // Hover-effekt
+  circle.addEventListener("mouseover", () => {
+    circle.setAttribute("fill", "white");
+  });
+
+  circle.addEventListener("mouseout", () => {
+    circle.setAttribute("fill", "red");
+  });
+
+  // Klik for at vise info
+  circle.addEventListener("click", () => {
+    const title = circle.getAttribute("data-title") || "Ingen titel";
+    const text = circle.getAttribute("data-text") || "Ingen tekst";
+    document.querySelector(".info-text h2").textContent = title;
+    document.querySelector(".info-text article p").textContent = text;
+  });
+});
+
+// document.querySelector("#circle2").addEventListener("mouseover", headHighlight);
+// document.querySelector("#circle2").addEventListener("mouseout", headunHighlight);
+// document.querySelector("#circle2").addEventListener("click", headInfo);
+
+// function headHighlight() {
+//   console.log("headHighlight");
+//   document.querySelector("#circle2").style.fill = "white";
+// }
+
+// function headunHighlight() {
+//   console.log("headunHighlight");
+//   document.querySelector("#circle2").style.fill = "red";
+// }
+
+// function headInfo() {
+//   console.log("headInfo");
+//   document.querySelector(".info-text > h2").textContent = "Hovedet!";
+//   document.querySelector(".info-text > article > p").textContent = "lorem ipsum";
+// }
